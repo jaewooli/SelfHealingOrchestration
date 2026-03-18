00260363ed9662b00121d1cc816658f8561eb26a
jaewooli <jaeu1341@naver.com>
2026-03-18 18:04:08 +0900

    Update: GuardDuty.sh

--- GuardDuty.sh
+++ GuardDuty.sh
@@ -4,16 +4,17 @@ set -e
 
 TARGET_IP="${1:-127.0.0.1}"
 PORT="${2:-8080}"
-LOOP="${3:10}"
+LOOP="${3:-10}"
 
 
 echo "Target: http://${TARGET_IP}:${PORT}"
 
 echo "Start"
-for i in {0..${LOOP}}
+while [ $LOOP -ge 0 ]
     do 
     curl "http://${TARGET_IP}:${PORT}/call?target=146.112.59.12"
     echo -e ""
+    LOOP=$(($LOOP - 1))
 done
 
 echo "Done."
